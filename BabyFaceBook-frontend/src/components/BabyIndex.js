79c49be8772a3e787439df26b053b6f30a99dad4
Aidan McBride <aidankmcbride@gmail.com>
2020-11-22 18:30:20 -0500

    add key to map

--- BabyFaceBook-frontend/src/components/BabyIndex.js
+++ BabyFaceBook-frontend/src/components/BabyIndex.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { useHistory } from 'react-router'
 
 export default function BabyIndex (props) {
-    console.log(props.handleClick)
+
     const babies = Object.values(props.babies)
     let history = useHistory()
 
     return(
         <div>
-        {babies.map( baby => (
+        {babies.map( (baby, index) => (
         <div  
+        key={index}
         onClick={() => props.handleClick(baby.id) }>
             <h3>{baby.name}</h3>
             <img src={baby.img} />
